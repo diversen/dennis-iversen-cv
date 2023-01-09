aa5230c3da09b40d03ac30c20bb5a07aeeef8c0d
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-09 13:33:23 +0100

    up

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 pandoc -s -o cv.pdf cv.md --from markdown+raw_html --pdf-engine=xelatex
 pandoc -s -o ansøgning.pdf ansøgning.md --from markdown+raw_html --pdf-engine=xelatex 
+pandoc -s -o standard.pdf standard.md --from markdown+raw_html --pdf-engine=xelatex 
